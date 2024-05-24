c526887c44f5691b7d779e54263797362daa2822
Zeeshan Ali <zeeshanali6401@gmail.com>
2024-05-24 22:38:19 +0500

    widgets added to supervisor panel

---
+++ app/Filament/Supervisor/Widgets/DivisionStatsOverview.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Supervisor\Widgets;
+
+use App\Models\DIR;
+use Carbon\Carbon;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class DivisionStatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $currentDate = Carbon::today(); // Get the current date
+
+        return [
+            Stat::make('City', DIR::where('division', 'city')
+                ->whereDate('created_at', $currentDate)
+                ->count())
+                ->description('All City DIRs')
+                ->color('success'),
+                Stat::make('Sader', DIR::where('division', 'sader')
+                ->whereDate('created_at', $currentDate)
+                ->count())
+                ->description('All Sader DIRs')
+                ->color('success'),
+                Stat::make('Civil Lines', DIR::where('division', 'Civil_lines')
+                ->whereDate('created_at', $currentDate)
+                ->count())
+                ->description('All Civil Lines DIRs')
+                ->color('success'),
+                Stat::make('Iqbal Town', DIR::where('division', 'iqbal_town')
+                ->whereDate('created_at', $currentDate)
+                ->count())
+                ->description('All Iqbal Town DIRs')
+                ->color('success'),
+                Stat::make('Model Town', DIR::where('division', 'model_town')
+                ->whereDate('created_at', $currentDate)
+                ->count())
+                ->description('All Model Town DIRs')
+                ->color('success'),
+                Stat::make('Cantt', DIR::where('division', 'cantt')
+                ->whereDate('created_at', $currentDate)
+                ->count())
+                ->description('All Cantt DIRs')
+                ->color('success'),
+        ];
+    }
+}
