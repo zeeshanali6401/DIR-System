c2cd1f1981cd748967a52addc00e72292e0fdd2d
Zeeshan Ali <zeeshanali6401@gmail.com>
2024-07-03 19:35:26 +0500

    Comitted

--- app/Filament/Widgets/LineChart.php
+++ app/Filament/Widgets/LineChart.php
@@ -39,36 +39,6 @@ class LineChart extends ChartWidget
             'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         ];
     }
-
-
-
-    // protected function getData(): array
-    // {
-    //     $january = DIR::whereMonth('case_date_time', '=', 1)->count();
-    //     $fabuary = DIR::whereMonth('case_date_time', '=', 2)->count();
-    //     $march = DIR::whereMonth('case_date_time', '=', 3)->count();
-    //     $april = DIR::whereMonth('case_date_time', '=', 4)->count();
-    //     $may = DIR::whereMonth('case_date_time', '=', 5)->count();
-    //     $jun = DIR::whereMonth('case_date_time', '=', 6)->count();
-    //     $july = DIR::whereMonth('case_date_time', '=', 7)->count();
-    //     $august = DIR::whereMonth('case_date_time', '=', 8)->count();
-    //     $september = DIR::whereMonth('case_date_time', '=', 9)->count();
-    //     $october = DIR::whereMonth('case_date_time', '=', 10)->count();
-    //     $november = DIR::whereMonth('case_date_time', '=', 11)->count();
-    //     $december = DIR::whereMonth('case_date_time', '=', 12)->count();
-
-    //     return [
-    //         'datasets' => [
-    //             [
-    //                 'label' => 'Cases',
-    //                 'data' => [$january, $fabuary, $march, $april, $may, $jun, $july, $august, $september, $october, $november, $december],
-    //                 'backgroundColor' => '#36A2EB',
-    //                 'borderColor' => '#9BD0F5',
-    //             ],
-    //         ],
-    //         'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    //     ];
-    // }
     public function getDescription(): ?string
     {
         return 'The number of DIR recorded per month.';
